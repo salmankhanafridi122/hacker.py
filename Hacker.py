97852f4e9de7704d6f6bb8d3db2fc81012309c9f
salmankhanafridi122 <95679020+salmankhanafridi122@users.noreply.github.com>
2022-01-04 13:22:59 +0500

    Create Hacker.py
    Git clone 
    https://github.com/salmankhanafridi122/hacker.py.git
    Cd hacker.py
    Python2 hacker.py

---
+++ Hacker.py
@@ -0,0 +1,31 @@
+<h1 align="center">Hi 👋, I'm HACKER-MSK</h1>
+<h3 align="center">DREAM TO BECOME A TRILLION LIVE IN HELL</h3>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=HACKER-MSK&label=Profile%20views&color=0e75b6&style=flat" alt="HACKER-MSK" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=HACKER-MSK" alt="HACKER-MSK" /></a> </p>
+
+- 🔭 I’m currenty working on my Facebook cloning projects **Facebook Cloning**
+
+- 👨‍💻 All of my projects are available at [https://github.com/HACKER-MSK](https://github.com/HACKER-MSK)
+
+- 📫 How to reach me **https://www.facebook.com/HACKER-MSK
+
+- 📄 Know about my experiences [https://www.facebook.com/HACKER-MSK ](https://www.facebook.com/HACKER-MSK)
+
+- ⚡ Fun fact **I think I am mad**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://fb.com/HACKER-MSK" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="HACKER-MSK" height="30" width="40" /></a>
+<a href="https://instagram.com/HACKER-MSK" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="HACKER-MSK" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://developer.android.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg" alt="android" width="40" height="40"/> </a> <a href="https://golang.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg" alt="go" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://unity.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg" alt="unity" width="40" height="40"/> </a> </p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=HACKER-MSK&show_icons=true&locale=en&layout=compact" alt="HACKER-MSK" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=HACKER-MSK&show_icons=true&locale=en" alt="HACKER-MSK" /></p>
+
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=HACKER-MSK&" alt="HACKER-MSK" /></p>
